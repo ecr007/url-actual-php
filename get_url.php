ce556b1a15d8dc0241ea2f39ba79bfe58d056b22
ecr007 <evercuevas1000@gmail.com>
2014-01-30 11:03:15 -0800

    Update get_url.php

--- get_url.php
+++ get_url.php
@@ -1,5 +1,7 @@
 function getCurrentUrl(){  
-		$domain = $_SERVER['HTTP_HOST'];	//Nombre del dominion  
-		$url = "http://" . $domain . $_SERVER['REQUEST_URI'];	  // Direccion despus del dominio URI
-		return $url;	  
+	$domain = $_SERVER['HTTP_HOST']; //Nombre del dominion  
+
+	$url = "http://" . $domain . $_SERVER['REQUEST_URI']; // Direccion despus del dominio URI
+
+	return $url;	  
 }
